686cd726cbfd3ae61aec8b5536d43ca257565aa6
turly221 <feifan@scntist.com>
2024-12-08 05:48:13 +0000

    commit patch 21268367

--- src/libopensc/pkcs15-prkey.c
+++ src/libopensc/pkcs15-prkey.c
@@ -177,6 +177,13 @@ int sc_pkcs15_decode_prkdf_entry(struct sc_pkcs15_card *p15card,
 	memset(gostr3410_params, 0, sizeof(gostr3410_params));
 
 	r = sc_asn1_decode_choice(ctx, asn1_prkey, *buf, *buflen, buf, buflen);
+	if (r < 0) {
+		/* This might have allocated something. If so, clear it now */
+		if (asn1_com_prkey_attr->flags & SC_ASN1_PRESENT &&
+			asn1_com_prkey_attr[0].flags & SC_ASN1_PRESENT) {
+			free(asn1_com_prkey_attr[0].parm);
+		}
+	}
 	if (r == SC_ERROR_ASN1_END_OF_CONTENTS)
 		return r;
 	SC_TEST_RET(ctx, SC_LOG_DEBUG_NORMAL, r, "ASN.1 decoding failed");
